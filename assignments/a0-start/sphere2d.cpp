f4133091d5268da36523b23ddcbac2989d6e4fa5
David Dinh <dndinh7@gmail.com>
2021-09-07 12:10:21 -0400

    I managed to get the overlapping spheres in the 2d file, and I think I got the sphere to translate in the 3d, however the camera is fixed on the object, so it is difficult for me to see if the direction is random although the position seems to indicate that it is indeed random? Need to fix that.

--- assignments/a0-start/sphere2d.cpp
+++ assignments/a0-start/sphere2d.cpp
@@ -6,7 +6,6 @@ class Sphere2D : public atkui::Framework {
  public:
   Sphere2D() : atkui::Framework(atkui::Orthographic) {
   }
-
   virtual void scene() {
     // colors are RGB triplets in range [0,1]
     setColor(vec3(0,1,0));
@@ -16,6 +15,14 @@ class Sphere2D : public atkui::Framework {
     double y = height() * 0.5;
     double radius = 100; 
     drawSphere(vec3(x,y,0), radius);
+
+    // color to be red
+    setColor(vec3(1, 0, 0));
+
+    // draw a sphere with double the radius, but behind the center of screen
+    radius = 165;
+    drawSphere(vec3(x, y, -50), radius);
+
   }
 };
 
--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -5,6 +5,8 @@ using glm::vec3;
 class Sphere3D : public atkui::Framework {
  public:
   Sphere3D() : atkui::Framework(atkui::Perspective) {
+      position = vec3(0);
+      velocity = vec3(0);
   }
 
   virtual void scene() {
@@ -14,7 +16,24 @@ class Sphere3D : public atkui::Framework {
     // draw a sphere at center of the world
     float radius = 50.0;
     drawSphere(vec3(0), radius);
+    position += velocity * dt();
+
+    renderer.translate(position);
+    
   }
+
+  void keyUp(int key, int mods) {
+      if (key == GLFW_KEY_SPACE) {
+          velocity = agl::randomUnitVector() * 30.0f;
+      }
+      if (key == GLFW_KEY_R) {
+          position = vec3(0);
+          velocity = vec3(0);
+      }
+  }
+private:
+    vec3 position;
+    vec3 velocity;
 };
 
 int main(int argc, char** argv)

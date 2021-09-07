d89d3f9f36174f8d614b3cf07e0d9191568c2a50
David Dinh <dndinh7@gmail.com>
2021-09-07 15:02:07 -0400

    I was able to edit my code so that I wasn't 'translating' the sphere. The sphere also does move in a random direction when pressing space. Should we allow for multiple space pressing?

--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -5,26 +5,31 @@ using glm::vec3;
 class Sphere3D : public atkui::Framework {
  public:
   Sphere3D() : atkui::Framework(atkui::Perspective) {
+      
+  }
+
+  virtual void setup() {
       position = vec3(0);
       velocity = vec3(0);
   }
 
   virtual void scene() {
+    // update position based on time
+    position += velocity * dt();
+
     // colors are RGB triplets in range [0,1]
     setColor(vec3(0,1,0));
 
     // draw a sphere at center of the world
     float radius = 50.0;
-    drawSphere(vec3(0), radius);
-    position += velocity * dt();
-
-    renderer.translate(position);
+    drawSphere(position, radius);
+    
     
   }
 
   void keyUp(int key, int mods) {
       if (key == GLFW_KEY_SPACE) {
-          velocity = agl::randomUnitVector() * 30.0f;
+          velocity = agl::randomUnitVector() * 40.0f;
       }
       if (key == GLFW_KEY_R) {
           position = vec3(0);

315ec54f3cceb8e1ab3306e640db815325a8ab4c
David Dinh <dndinh7@gmail.com>
2021-09-09 17:06:42 -0400

    I coded the eyes. It seems to work as intended, but it was a very fast draft.

--- assignments/a1-hello/eyes.cpp
+++ assignments/a1-hello/eyes.cpp
@@ -7,8 +7,30 @@ class Eyes : public atkui::Framework
     Eyes() : atkui::Framework(atkui::Orthographic) {
     }
 
-    virtual void scene() {
+    virtual void setup() {
+        sizeBig = 150.0f;
+        sizeSmall = 35.0f;
+        radiusOrbit = 45.0f;
     }
+
+    virtual void scene() {
+        // need to create 4 spheres
+        // 2 are static and the other 2 are dynamic
+        setColor(vec3(1, 1, 1));
+
+        drawSphere(vec3(0.30 * width(), 0.5 * height(), -100), sizeBig);
+        drawSphere(vec3(0.70 * width(), 0.5 * height(), -100), sizeBig);
+
+        setColor(vec3(0, 0, 0));
+
+        drawSphere(vec3(0.30 * width() + radiusOrbit * sin(elapsedTime()), 0.5 * height(), 0), sizeSmall);
+        drawSphere(vec3(0.70 * width() + radiusOrbit * sin(elapsedTime()), 0.5 * height(), 0), sizeSmall);
+    }
+private:
+    float sizeBig;
+    float sizeSmall;
+    float radiusOrbit;
+
 };
 
 int main(int argc, char** argv) {

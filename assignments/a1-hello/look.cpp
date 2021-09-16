ca04e944068d793b4274a5b3da7435a4844d2e90
David Dinh <dndinh7@gmail.com>
2021-09-16 12:24:48 -0400

    I think I created an animation where the eyes follow the cursor if the mouse button is held down. Should the pupils teleport if the mouse is not down and pressed in a different location? Are the pupils always a set distance from the center, i.e. when we press in the center of the eyes, do the pupils ever move to the center?

--- assignments/a1-hello/look.cpp
+++ assignments/a1-hello/look.cpp
@@ -10,12 +10,44 @@ class Look : public atkui::Framework {
   virtual void setup() {
     _mouseX = width() * 0.5;
     _mouseY = height() * 0.5;
+    pivotLeftPupil = vec3(0.30 * width(), 0.5 * height(), 0);
+    pivotRightPupil = vec3(0.70 * width(), 0.5 * height(), 0);
+    theta = atan2(_mouseX, _mouseY);
+    a = 42.5f;
   }
 
   virtual void scene() {
+
+    setColor(vec3(1));
+
+    drawSphere(vec3(0.30 * width(), 0.5 * height(), -100), 150.0f);
+    drawSphere(vec3(0.70 * width(), 0.5 * height(), -100), 150.0f);
+
+    setColor(vec3(0));
+
+    vec3 pivot = vec3(_mouseX, _mouseY, 0) - pivotLeftPupil;
+    theta = atan2(pivot.y, pivot.x);
+
+    drawSphere(pivotLeftPupil + a*vec3(cos(theta), sin(theta), 0), 35.0f);
+
+    pivot = vec3(_mouseX, _mouseY, 0) - pivotRightPupil;
+    theta = atan2(pivot.y, pivot.x);
+
+    drawSphere(pivotRightPupil + a * vec3(cos(theta), sin(theta), 0), 35.0f);
+
+
+
+    if (mouseIsDown(GLFW_MOUSE_BUTTON_LEFT)) {
+        vec2 mousePos = mousePosition();
+        mouseMove(mousePos.x, mousePos.y);
+        theta = atan2(_mouseX, _mouseY);
+    }
     vec3 target = vec3(_mouseX, _mouseY, 0);
     setColor(vec3(1,0,0));
-    drawSphere(target, 5);
+    drawSphere(target, 7.5);
+
+
+
   }
 
   void mouseMove(int x, int y) {
@@ -23,9 +55,14 @@ class Look : public atkui::Framework {
     _mouseY = height() - y;
   }
 
+  
  private:
   int _mouseX;
   int _mouseY;
+  vec3 pivotLeftPupil;
+  vec3 pivotRightPupil;
+  float theta;
+  float a; // this will be the distance from the pivot
 };
 
 int main(int argc, char** argv) {

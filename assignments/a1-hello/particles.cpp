71263d9686174dc78dd4383abf84b57ce6540973
David Dinh <dndinh7@gmail.com>
2021-09-17 19:20:45 -0400

    I changed the speed of the particles to be faster and added documentation

--- assignments/a1-hello/particles.cpp
+++ assignments/a1-hello/particles.cpp
@@ -5,8 +5,8 @@ using namespace glm;
 class Dot {
 public:
 	Dot(float widthScreen, float heightScreen, vec3 direction) : position{ vec3(agl::random(0, widthScreen), agl::random(0, heightScreen), agl::random(-5.0f, 5.0f)) }, // add random z positions to have random overlapping
-		color{vec3(0.3f + agl::random(-0.3f, 0.3f), 0.7f + agl::random(-0.3f, 0.3f), 0.7 + agl::random(-0.3f, 0.3f))},
-		velocity{direction*agl::random(50.0f, 100.0f)} {
+		color{vec3(0.3f + agl::random(-0.3f, 0.3f), 0.7f + agl::random(-0.3f, 0.3f), 0.7 + agl::random(-0.3f, 0.3f))}, // jitter the colors
+		velocity{direction*agl::random(100.0f, 225.0f)} {
 	}
 
 	vec3 position;
@@ -22,7 +22,7 @@ class Particles : public atkui::Framework
   }
 
   virtual void setup() {
-	  count = 200;
+	  count = 250;
 	  direction = agl::randomUnitVector();
 	  ifHorizVelPos = direction.x > 0;
 	  ifVertVelPos  = direction.y > 0;
@@ -39,17 +39,16 @@ class Particles : public atkui::Framework
 
 	  float radius = 30.0f;
 	  for (auto& dot : collection) {
-		  if (ifHorizVelPos) {
+		  if (ifHorizVelPos) { // if we are moving in a positive x direction, we will wrap back to x=0
 			  dot.position.x = (dot.position.x > widthScreen) ? 0 : dot.position.x + dot.velocity.x * dt();
 		  }
-		  else {
+		  else { // we are moving in negative x direction, we will wrap to the screen width
 			  dot.position.x = (dot.position.x < 0) ? widthScreen : dot.position.x + dot.velocity.x * dt();
 		  }
-		  if (ifVertVelPos) {
-
+		  if (ifVertVelPos) { // if we are moving in the positive y direction, we will wrap to y=0
 			  dot.position.y = (dot.position.y > heightScreen) ? 0 : dot.position.y + dot.velocity.y * dt();
 		  }
-		  else {
+		  else { // we are moving in the negative y direction, we will wrap to height width
 			  dot.position.y = (dot.position.y < 0) ? heightScreen : dot.position.y + dot.velocity.y * dt();
 		  }
 		  setColor(dot.color);
@@ -59,9 +58,9 @@ class Particles : public atkui::Framework
 
 private:
 	std::vector<Dot> collection;
-	vec3 direction;
-	bool ifHorizVelPos;
-	bool ifVertVelPos;
+	vec3 direction; // this will be a random direction that all particles will move
+	bool ifHorizVelPos; // true if moving in positive x direction, false elsewise
+	bool ifVertVelPos; // true if moving in positive y direction, false elsewise
 	int count; // this is the variable for how many dots we want on the screen
 	float widthScreen;
 	float heightScreen;

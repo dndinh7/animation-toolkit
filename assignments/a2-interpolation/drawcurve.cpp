7032d3da1979ab1f1af929a680ee2956d5735ca1
David Dinh <dndinh7@gmail.com>
2021-09-23 12:33:42 -0400

    I was able to do the algorithms to create the curve, however I need to figure out how to draw the line when the keys are pressed.

--- assignments/a2-interpolation/drawcurve.cpp
+++ assignments/a2-interpolation/drawcurve.cpp
@@ -8,6 +8,27 @@ class DrawCubic : public atkui::Framework
   DrawCubic() : atkui::Framework(atkui::Orthographic, 400, 400) {
   }
 
+  virtual void setup() {
+      float interval = 1.0f / (N + 1);
+      pointsBernstein.push_back(B0);
+      for (int i = 1; i <= N; i++) {
+          float t = interval * i;
+          vec3 point = (1.0f - t) * (1.0f - t) * (1.0f - t) * B0 
+              + 3 * t * (1.0f - t) * (1.0f - t) * B1 
+              + 3 * t * t * (1.0f - t) * B2 
+              + t * t * t * B3;
+          pointsBernstein.push_back(point);
+      }
+      pointsBernstein.push_back(B3);
+
+      pointsCasteljau.push_back(B0);
+      for (int i = 1; i <= N; i++) {
+          float t = interval * i;
+          pointsCasteljau.push_back(LERPALL(B0, B1, B2, B3, t));
+      }
+      pointsCasteljau.push_back(B3);
+  }
+
   virtual void scene()
   {
     setColor(vec3(0,0,1));
@@ -17,22 +38,60 @@ class DrawCubic : public atkui::Framework
     setColor(vec3(1,1,0));
     drawSphere(B1, 10);
     drawSphere(B2, 10);
+
+
+    /*setColor(vec3(1));
+    for (int i = 0; i < pointsBernstein.size() - 1; i++) {
+        drawLine(pointsBernstein[i], pointsBernstein[i + 1]);
+    }*/
+
+    setColor(vec3(1, 1, 0));
+    for (int i = 0; i < pointsCasteljau.size() - 1; i++) {
+        drawLine(pointsCasteljau[i], pointsCasteljau[i + 1]);
+    }
+
   }
 
   void keyUp(int key, int mod) {
     if (key == GLFW_KEY_1) {
       // draw Bernstein
+        /*setColor(vec3(1));
+        for (int i = 0; i < pointsBernstein.size()-1; i++) {
+            drawLine(pointsBernstein[i], pointsBernstein[i + 1]);
+        }*/
+
+        std::cout << pointsBernstein[0] << std::endl;
     }
     else if (key == GLFW_KEY_2) {
       // draw Casteljau
+        std::cout << pointsCasteljau.size() << std::endl;
+        //std::cout << pointsCasteljau[0] << std::endl;
     }
   }
 
+  vec3 LERP(const vec3& b1, const vec3 b2, float t) {
+      return b1 * (1 - t) + b2 * t;
+  }
+
+  vec3 LERPALL(const vec3& b0, const vec3& b1, const vec3& b2, const vec3& b3, float t) {
+      vec3 b01 = LERP(b0, b1, t);
+      vec3 b11 = LERP(b1, b2, t);
+      vec3 b21 = LERP(b2, b3, t);
+
+      vec3 b02 = LERP(b01, b11, t);
+      vec3 b12 = LERP(b11, b21, t);
+
+      return LERP(b02, b12, t);
+  }
+
  private:
+  int N = 40; // number of intermediate points between B0 and B3
   vec3 B0 = vec3(100,  50, 0);
   vec3 B1 = vec3(150, 200, 0);
   vec3 B2 = vec3(250, 100, 0);
   vec3 B3 = vec3(300, 300, 0);
+  std::vector<vec3> pointsBernstein;
+  std::vector<vec3> pointsCasteljau;
 };
 
 int main(int argc, char **argv) {

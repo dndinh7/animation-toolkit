a1a90ea4959660758a28ffebe18c3f079736f2b4
David Dinh <dndinh7@gmail.com>
2021-09-30 09:36:59 -0400

    I implemented both functions for the LinearInterpolator.

--- assignments/a3-splines/interpolator_linear.h
+++ assignments/a3-splines/interpolator_linear.h
@@ -14,12 +14,31 @@ class InterpolatorLinear : public Interpolator
 public:
     InterpolatorLinear() : Interpolator("Linear") {}
     virtual glm::vec3 interpolate(int segment, double u) const {
-       // todo: your code here
-       return glm::vec3(0);
+        // todo: your code here
+
+        // given a segment n, the two end points are n and n+1
+        glm::vec3 p0= getControlPoint(segment);
+        glm::vec3 p1= getControlPoint(segment + 1);
+
+        glm::vec3 p = (1 - (float)u) * p0 + (float)u * p1;
+
+
+
+        return p;
     }
 
     virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
-       // todo: your code here
+        // todo: your code here
+
+        if (keys.size() == 0 || keys.size() == 1) {
+            clearControlPoints();
+        }
+        else {
+            clearControlPoints();
+            for (int i = 0; i < keys.size(); i++) {
+                mCtrlPoints.push_back(keys[i]);
+            }
+        }
     }
 };
 

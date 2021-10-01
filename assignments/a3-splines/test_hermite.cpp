8a93f73a634e2470fede1a24f7166fe486dc6a4b
David Dinh <dndinh7@gmail.com>
2021-10-01 13:41:10 -0400

    I added a small test case.

--- assignments/a3-splines/test_hermite.cpp
+++ assignments/a3-splines/test_hermite.cpp
@@ -4,6 +4,52 @@
 using namespace glm;
 using namespace std;
 
+void test() {
+    std::cout << "\n----- Starting Individual Testing-----" << std::endl;
+
+
+    std::cout << "Testing key1: 6 control points" << std::endl;
+    std::vector<vec3> key1;
+    key1.push_back(vec3(0, 0, 0));
+    key1.push_back(vec3(1, 1, 0));
+    key1.push_back(vec3(2, 0, 0));
+
+    InterpolatorHermite interp1;
+    interp1.computeControlPoints(key1);
+
+    for (int i = 0; i < interp1.getNumControlPoints(); i++) {
+        std::cout << "control points: " << to_string(interp1.getControlPoint(i)) << std::endl;
+    }
+
+    vec3 val1;
+
+
+    for (int i = 0; i < interp1.getNumControlPoints()/2 - 1; i++) {
+        for (int j = 0; j < 5; j++) {
+            val1 = interp1.interpolate(i, 0.25f * j);
+            std::cout << "(natural) interpolate(" << std::to_string(i) << ", " << std::to_string(0.25f * j) << ") = " << to_string(val1) << std::endl;
+        }
+    }
+
+    
+    interp1.setClamped(true);
+    interp1.setClampedDirection(vec3(1.0, 0.0, 0.0));
+    interp1.computeControlPoints(key1);
+
+    for (int i = 0; i < interp1.getNumControlPoints(); i++) {
+        std::cout << "control points: " << to_string(interp1.getControlPoint(i)) << std::endl;
+    }
+
+    for (int i = 0; i < interp1.getNumControlPoints()/2 - 1; i++) {
+        for (int j = 0; j < 5; j++) {
+            val1 = interp1.interpolate(i, 0.25f * j); // only 1 segment exists
+            std::cout << "(clamped) interpolate(" << std::to_string(i) << ", " << std::to_string(0.25f * j) << ") = " << to_string(val1) << std::endl;
+        }
+    }
+
+
+}
+
 int main()
 {
    std::vector<vec3> keys;
@@ -50,5 +96,7 @@ int main()
 
    value = hermite.interpolate(2, 0.5);
    std::cout << "(natural) interpolate(2, 0.5) = " << to_string(value) << std::endl;
+
+   test();
 }
 

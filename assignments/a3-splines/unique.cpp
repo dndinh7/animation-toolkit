09f9a5f22894a5cb97d628eb220e476921b127fe
David Dinh <dndinh7@gmail.com>
2021-10-01 23:58:27 -0400

    I am still trying to debug, but this will be the last push before midnight. Please grade this version if I would get points taken off for the commits after midnight.

--- assignments/a3-splines/unique.cpp
+++ assignments/a3-splines/unique.cpp
@@ -17,22 +17,29 @@ public:
         splinePath.addInterpolationType("Hermite", &hermite);
         splineColor.addInterpolationType("CatmullRom", &catmullrom);
 
+        splinePath.setInterpolationType("Hermite");
+        splineSize.setInterpolationType("Linear");
+        splineColor.setInterpolationType("CatmullRom");
+
         for (int i = 0; i < numVecs; i++) {
             splinePath.appendKey(timeCount, vec3(agl::random(10, screenWidth - 10), agl::random(10, screenHeight - 10), 0));
             splineColor.appendKey(timeCount, vec3(agl::random(), agl::random(), agl::random()));
             splineSize.appendKey(timeCount, vec3(agl::random(10.0f, 50.0f), agl::random(10.0f, 50.0f), 0));
             timeCount++;
         }
+        splinePath.computeControlPoints();
+        splineSize.computeControlPoints();
+        splineColor.computeControlPoints();
+    }
 
+    void lol() {
+        std::cout << splinePath.getValue(0) << std::endl;
         std::cout << splineColor.getValue(0) << std::endl;
         std::cout << splineSize.getValue(0) << std::endl;
-        std::cout << splinePath.getValue(0) << std::endl;
-        
 
+        std::cout << splinePath.getValue(0) << std::endl;
         std::cout << splineColor.getValue(0) << std::endl;
-        std::cout << splinePath.getValue(0) << std::endl;
         std::cout << splineSize.getValue(0) << std::endl;
-
     }
 
     Spline splinePath;
@@ -52,19 +59,18 @@ public:
       for (int i = 0; i < numSquares; i++) {
           squareCollection.push_back(Square(width(), height(), numPointsInSpline));
       }
+      std::cout << squareCollection[0].splinePath.getValue(0) << std::endl;
   }
 
   virtual void setup() {
-      std::cout << squareCollection[0].splineColor.getValue(0) << std::endl;
+      squareCollection[0].lol();
   }
 
   virtual void scene() {
-      /*std::cout << squareCollection[0].splineColor.getValue(0) << std::endl;
-      std::cout << squareCollection[0].splinePath.getValue(0) << std::endl;
-      std::cout << squareCollection[0].splineSize.getValue(0) << std::endl;*/
+      //std::cout << squareCollection[0].splinePath.getValue(0) << std::endl;
       float t = fmod(elapsedTime(), 5.0f);
       for (int i = 0; i < squareCollection.size(); i++) {
-          setColor(squareCollection[i].splineColor.getValue(0));
+          //setColor(squareCollection[i].splineColor.getValue(t));
           //drawCube(squareCollection[i].splinePath.getValue(t), squareCollection[i].splineSize.getValue(t));
       }
       

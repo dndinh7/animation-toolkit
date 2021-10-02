e28fb11c0c847a69544c734359c77b192b8f5672
David Dinh <dndinh7@gmail.com>
2021-10-02 00:54:43 -0400

    I am really sorry that this is late. The spline objects I tried creating in my Square objects kept deconstructing once they went of scope. The squares were also deconstructing as well after the for loop. I was really confused as to why it was happening. I then realized I had to dynamically allocate the both objects and create deconstructors for both the Unique3 and Square class. I also had to make sure that squareCollection was a vector of pointers. Ahh it was a mess to deal with since I'm not that best at dealing with pointers. Anyway, here is the final product of moving, changing color and size squares based on each spline.

--- assignments/a3-splines/unique.cpp
+++ assignments/a3-splines/unique.cpp
@@ -7,44 +7,37 @@ using namespace glm;
 
 class Square {
 public:
-    Square(float screenWidth, float screenHeight, int numVecs)
+    Square(float screenWidth, float screenHeight, int numVecs) : splinePath(new Spline()), splineColor(new Spline()), splineSize(new Spline())
     {
-        splinePath = Spline();
-        splineColor = Spline();
-        splineSize = Spline();
 
-        splineSize.addInterpolationType("Linear", &linear);
-        splinePath.addInterpolationType("Hermite", &hermite);
-        splineColor.addInterpolationType("CatmullRom", &catmullrom);
+        splineSize->addInterpolationType("Linear", &linear);
+        splinePath->addInterpolationType("Hermite", &hermite);
+        splineColor->addInterpolationType("CatmullRom", &catmullrom);
 
-        splinePath.setInterpolationType("Hermite");
-        splineSize.setInterpolationType("Linear");
-        splineColor.setInterpolationType("CatmullRom");
+        splinePath->setInterpolationType("Hermite");
+        splineSize->setInterpolationType("Linear");
+        splineColor->setInterpolationType("CatmullRom");
 
         for (int i = 0; i < numVecs; i++) {
-            splinePath.appendKey(timeCount, vec3(agl::random(10, screenWidth - 10), agl::random(10, screenHeight - 10), 0));
-            splineColor.appendKey(timeCount, vec3(agl::random(), agl::random(), agl::random()));
-            splineSize.appendKey(timeCount, vec3(agl::random(10.0f, 50.0f), agl::random(10.0f, 50.0f), 0));
+            splinePath->appendKey(timeCount, vec3(agl::random(10, screenWidth - 10), agl::random(10, screenHeight - 10), 0));
+            splineColor->appendKey(timeCount, vec3(agl::random(), agl::random(), agl::random()));
+            splineSize->appendKey(timeCount, vec3(agl::random(15.0f, 80.0f), agl::random(10.0f, 50.0f), 0));
             timeCount++;
         }
-        splinePath.computeControlPoints();
-        splineSize.computeControlPoints();
-        splineColor.computeControlPoints();
+        splinePath->computeControlPoints();
+        splineSize->computeControlPoints();
+        splineColor->computeControlPoints();
     }
 
-    void lol() {
-        std::cout << splinePath.getValue(0) << std::endl;
-        std::cout << splineColor.getValue(0) << std::endl;
-        std::cout << splineSize.getValue(0) << std::endl;
-
-        std::cout << splinePath.getValue(0) << std::endl;
-        std::cout << splineColor.getValue(0) << std::endl;
-        std::cout << splineSize.getValue(0) << std::endl;
+    ~Square() { // since Spline has to live longer than in the scope of Square, we must delete it after Square goes out of scope
+        delete splinePath;
+        delete splineColor;
+        delete splineSize;
     }
 
-    Spline splinePath;
-    Spline splineColor;
-    Spline splineSize;
+    Spline* splinePath;
+    Spline* splineColor;
+    Spline* splineSize;
     InterpolatorLinear linear;
     InterpolatorCatmullRom catmullrom;
     InterpolatorHermite hermite;
@@ -57,21 +50,33 @@ public:
   Unique3() : atkui::Framework(atkui::Orthographic) {
   
       for (int i = 0; i < numSquares; i++) {
-          squareCollection.push_back(Square(width(), height(), numPointsInSpline));
+          // need to dynamically allocated Square objects, so they do not go out of scope and deconstruct itself
+          squareCollection.push_back(new Square(width(), height(), numPointsInSpline)); 
       }
-      std::cout << squareCollection[0].splinePath.getValue(0) << std::endl;
+  }
+
+  ~Unique3() { // after we close the viewer, the object will deconstruct
+      for (int i = 0; i < squareCollection.size(); i++) {
+          Square* cube = squareCollection.at(i);
+          delete cube;
+      }
   }
 
   virtual void setup() {
-      squareCollection[0].lol();
   }
 
   virtual void scene() {
-      //std::cout << squareCollection[0].splinePath.getValue(0) << std::endl;
-      float t = fmod(elapsedTime(), 5.0f);
+      t = (forward) ? t + dt() : t - dt();
+      if (t <= 0) {
+          forward = true;
+      }
+      else if (t >= time) {
+          forward = false;
+      }
+
       for (int i = 0; i < squareCollection.size(); i++) {
-          //setColor(squareCollection[i].splineColor.getValue(t));
-          //drawCube(squareCollection[i].splinePath.getValue(t), squareCollection[i].splineSize.getValue(t));
+          setColor(squareCollection[i]->splineColor->getValue(t));
+          drawCube(squareCollection[i]->splinePath->getValue(t), squareCollection[i]->splineSize->getValue(t));
       }
       
   }
@@ -79,12 +84,12 @@ public:
   
 
 private:
-    int numPointsInSpline = 5;
-    int numSquares = 1;
-    int timeCount = 0;
-    std::vector<Square> squareCollection;
-    float time= 5; // time I want it to take for each square to finish the interpolations of every point
-
+    int numPointsInSpline = 8; // change this to change the path and the time to traverse all the points
+    int numSquares = 25;
+    std::vector<Square *> squareCollection;
+    float time= numPointsInSpline-1; // time I want it to take for each square to finish the interpolations of every point
+    bool forward= true;
+    float t= 0; // this will be the t value to go through curves
 };
 
 int main(int argc, char** argv) {

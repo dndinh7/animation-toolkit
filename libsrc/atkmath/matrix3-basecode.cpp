5eea541ff3feeb7b3dabdb377f5fa9fe5858a6e5
David Dinh <dndinh7@gmail.com>
2021-10-04 11:38:09 -0400

    I finished implementing all toEulerAngles functions and they pas all the testing.

--- libsrc/atkmath/matrix3-basecode.cpp
+++ libsrc/atkmath/matrix3-basecode.cpp
@@ -35,11 +35,13 @@ Vector3 Matrix3::toEulerAnglesXYZ() const
 	float x;
 	float y = asin(mM[0][2]);
 	float z;
-	if (mM[0][2] == 1) { // this means Y has a rotation of 90
-		
+	if (mM[0][2] == 1) { // this means Y has a rotation of 90 or -90
+		z = 0;
+		x = atan2(mM[1][0], mM[1][1]);
 	}
-	else if (mM[0][2] == -1) { // this means Y has a rotation of -90
-
+	else if (mM[0][2] == -1) {
+		z = 0;
+		x = atan2(mM[2][1], mM[2][0]);
 	}
 	else {
 		z = atan2(-mM[0][1], mM[0][0]);
@@ -54,10 +56,12 @@ Vector3 Matrix3::toEulerAnglesXZY() const
 	float y;
 	float z = asin(-mM[0][1]);
 	if (mM[0][1] == 1) { // this means Z has a rotation of -90
-
+		y = 0;
+		x = atan2(-mM[2][0], -mM[1][0]);
 	}
 	else if (mM[0][1] == -1) { // this means Z has a rotation of 90
-
+		y = 0;
+		x = atan2(mM[2][0], mM[1][0]);
 	}
 	else {
 		y = atan2(mM[0][2], mM[0][0]);
@@ -71,16 +75,19 @@ Vector3 Matrix3::toEulerAnglesYXZ() const
 	float x = asin(-mM[1][2]);
 	float y;
 	float z;
-		if (mM[0][1] == 1) { // this means Z has a rotation of -90
+	if (mM[1][2] == 1) { // this means X has a rotation of -90
+		z = 0;
+		y = atan2(-mM[2][0], -mM[2][1]);
+	}
+	else if (mM[1][2] == -1) { // this means X has a rotation of 90
+		z = 0;
+		y = atan2(mM[0][1], mM[0][0]);
+	}
+	else {
+		y = atan2(mM[0][2], mM[2][2]);
+		z = atan2(mM[1][0], mM[1][1]);
+	}
 
-		}
-		else if (mM[0][1] == -1) { // this means Z has a rotation of 90
-
-		}
-		else {
-			z = atan2(mM[1][0], mM[1][1]);
-			y = atan2(mM[0][2], mM[2][2]);
-		}
 	return Vector3(x, y, z);
 }
 
@@ -89,12 +96,10 @@ Vector3 Matrix3::toEulerAnglesYZX() const
 	float x;
 	float y;
 	float z = asin(mM[1][0]);
-	if (mM[0][1] == 1) { // this means Z has a rotation of -90
-
-	}
-	else if (mM[0][1] == -1) { // this means Z has a rotation of 90
-
-	}
+	if (mM[1][0] == 1 || mM[1][0] == -1) { // this means Z has a rotation of 90 or -90
+		y = 0;
+		x = atan2(mM[2][1], mM[2][2]);
+	} 
 	else {
 		y = atan2(-mM[2][0], mM[0][0]);
 		x = atan2(-mM[1][2], mM[1][1]);
@@ -107,11 +112,9 @@ Vector3 Matrix3::toEulerAnglesZXY() const
 	float x = asin(mM[2][1]);
 	float y;
 	float z;
-	if (mM[0][1] == 1) { // this means Z has a rotation of -90
-
-	}
-	else if (mM[0][1] == -1) { // this means Z has a rotation of 90
-
+	if (mM[2][1] == 1 || mM[2][1] == -1) { // this means X has a rotation of 90 or -90
+		z = 0;
+		y = atan2(mM[0][2], mM[0][0]);
 	}
 	else {
 		z = atan2(-mM[0][1], mM[1][1]);

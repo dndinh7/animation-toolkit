1138529865784b510fbbad9f369855e55bc0669a
David Dinh <dndinh7@gmail.com>
2021-10-19 18:57:42 -0400

    I changed test_quat to use glm pi instead, since I was getting errors with M_PI.

--- assignments/a5-quat/test_quat.cpp
+++ assignments/a5-quat/test_quat.cpp
@@ -8,21 +8,22 @@
 using atkmath::Quaternion;
 using atkmath::Vector3;
 using atkmath::Rad2Deg;
+using namespace glm;
 
 int main()
 {
     Quaternion q;
     Vector3 v; double angle;
 
-    q.fromAxisAngle(Vector3(0,0,1), M_PI/4);
+    q.fromAxisAngle(Vector3(0,0,1), pi<float>()/4);
     q.toAxisAngle(v, angle);
     std::cout << "45 Z " << q << " " << v << " " << angle * Rad2Deg << std::endl;
 
-    q.fromAxisAngle(Vector3(0,1,0), M_PI/4);
+    q.fromAxisAngle(Vector3(0,1,0), pi<float>()/4);
     q.toAxisAngle(v, angle);
     std::cout << "45 Y " << q << " " << v << " " << angle * Rad2Deg << std::endl;
 
-    q.fromAxisAngle(Vector3(1,0,0), M_PI/4);
+    q.fromAxisAngle(Vector3(1,0,0), pi<float>()/4);
     q.toAxisAngle(v, angle);
     std::cout << "45 X " << q << " " << v << " " << angle * Rad2Deg << std::endl;
 

199accf5ccf0296e0d2314beded24e042a4c71f9
David Dinh <dndinh7@gmail.com>
2021-10-19 22:06:23 -0400

    I changed the tests to include pi<float>() instead of M_PI because it was not recognizing M_PI

--- assignments/a5-quat/test_quat_mat.cpp
+++ assignments/a5-quat/test_quat_mat.cpp
@@ -8,27 +8,28 @@ using atkmath::Quaternion;
 using atkmath::Matrix3;
 using atkmath::Vector3;
 using atkmath::Rad2Deg;
+using namespace glm;
 
 int main()
 {
     Quaternion q;
     Matrix3 m;
 
-    m.fromEulerAnglesXYZ(Vector3(0,0,M_PI/4));
+    m.fromEulerAnglesXYZ(Vector3(0,0,pi<float>()/4));
     q.fromMatrix(m);
     std::cout << "45 Z " << q << std::endl;
 
     m = q.toMatrix();
     std::cout << "45 Z" << std::endl << m << std::endl;
 
-    m.fromEulerAnglesXYZ(Vector3(0,M_PI/4,0));
+    m.fromEulerAnglesXYZ(Vector3(0, pi<float>()/4,0));
     q.fromMatrix(m);
     std::cout << "45 Y " << q << std::endl;
 
     m = q.toMatrix();
     std::cout << "45 Y " << std::endl << m << std::endl;
 
-    m.fromEulerAnglesXYZ(Vector3(M_PI/4,0,0));
+    m.fromEulerAnglesXYZ(Vector3(pi<float>()/4,0,0));
     q.fromMatrix(m);
     std::cout << "45 X " << q << std::endl;
 

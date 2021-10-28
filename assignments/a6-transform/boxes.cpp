3d56f360d39a71adf094b6b531bb4fc642497a1b
David Dinh <dndinh7@gmail.com>
2021-10-28 09:49:17 -0400

    transformed box2 from frame 2 into frame 1 and then frame 1 into global

--- assignments/a6-transform/boxes.cpp
+++ assignments/a6-transform/boxes.cpp
@@ -40,21 +40,19 @@ public:
 
       // Question 2
       Transform F21 = F10.inverse() * F20; // transform from frame 2 to frame 1
-      vec3 value = F21.transformPoint(vec3(3, 4, 1));
+      vec3 value = F21.transformPoint(d20);
       if (!once) std::cout << "Position of b2 relative to b1 is " << value << std::endl;
 
       // Question 3
       Transform F32 = F20.inverse() * F30; // transform from frame 3 to frame 2
-      value = F32.transformPoint(vec3(2, 3, 1));
+      value = F32.transformPoint(d30);
       if (!once) std::cout << "Position of b3 relative to b2 is " << value << std::endl;
 
       // Question 4
       // Compute a series of transforms that stack Box 2 onto Box 1
-      vec3 d40 = vec3(-2, 2, 1);
-      quat R40 = glm::angleAxis(0.f, vec3(0, 0, 1));
-      Transform F(R40, d40);
+      Transform F= F21.Translate(vec3(0, 2, 0));
       setColor(vec3(0.5, 0.5,0.5));
-      drawBox(F);
+      drawBox(F * F10); // convert from frame 2 into frame 1, and displace box by height of box
 
       once = true;
    }

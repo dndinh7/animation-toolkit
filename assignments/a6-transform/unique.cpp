a73679c3196a002f249dacb856b8ab276f031c77
David Dinh <dndinh7@gmail.com>
2021-10-29 01:09:40 -0400

    added unique file and updated the cmakelist to include it

---
+++ assignments/a6-transform/unique.cpp
@@ -0,0 +1,35 @@
+﻿#include "atkui/framework.h"
+#include "atkmath/constants.h"
+#include "atkmath/vector3.h"
+#include "atkmath/matrix3.h"
+#include "atkmath/quaternion.h"
+#include <string>
+#include <deque>
+#include <iostream>
+using namespace glm;
+using atkmath::Matrix3;
+using atkmath::Vector3;
+
+
+class Unique6 : public atkui::Framework {
+public:
+	Unique6() : atkui::Framework(atkui::Orthographic) {
+
+	}
+
+	virtual void setup() {
+		std::cout << "hell" << std::endl;
+	}
+
+	virtual void scene() {
+
+	}
+
+};
+
+int main(int argc, char** argv) {
+	Unique6 viewer;
+	viewer.run();
+	return 0;
+}
+

26c5f3768baf6a3c51d1c71b14071a81ee82d141
David Dinh <dndinh7@gmail.com>
2021-11-04 23:02:32 -0400

    I randomized them, but I'm not too sure what positioning them in the 4x3 grid means? How big is each square and how do I limit the grid?

--- assignments/a7-motion/thriller.cpp
+++ assignments/a7-motion/thriller.cpp
@@ -18,15 +18,30 @@ public:
       BVHReader reader;
       reader.load("../motions/Warrok/WarrokThriller.bvh", _skeleton, _motion);
 
-      vec3 position = vec3(0);
-      vec3 color = vec3(1,0,0);
-      float size = 1.0f;
-      _devil = Devil(position, color, size);
+      vec3 position;
+
+      vec3 color;
+      float size;
+
+      for (int i = 0; i < 12; i++) {
+          position = vec3(agl::random(-200, 200), 0, agl::random(-300, 300));
+          color = vec3(0.80f + agl::random(-0.20f, 0.20f), 0.20f + agl::random(-0.20f, 0.20f), 0.20f + agl::random(-0.20f, 0.20f));
+          size = agl::random(0.35f, 0.75f);
+
+          devils.push_back(Devil(position, color, size));
+      }
    }
 
    virtual void scene() {
-      if (!_paused) _motion.update(_skeleton, elapsedTime());
-      _devil.draw(_skeleton, *this);
+       if (!_paused) {
+           time += dt();
+           
+       }
+       _motion.update(_skeleton, time);
+
+       for (auto& devil : devils) {
+           devil.draw(_skeleton, *this);
+       }
    }
 
    virtual void keyUp(int key, int mods) {
@@ -37,7 +52,9 @@ protected:
    Motion _motion;
    Skeleton _skeleton;
    bool _paused = false;
+   std::vector<Devil> devils;
    Devil _devil;
+   float time= 0.0f;
 };
 
 int main(int argc, char** argv) {

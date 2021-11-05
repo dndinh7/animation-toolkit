9b310a88f51cab74214ae3d96dc2765f191d9e32
David Dinh <dndinh7@gmail.com>
2021-11-05 00:20:57 -0400

    implemented animation for butterfly and coordinate axes

--- assignments/a7-motion/butterfly.cpp
+++ assignments/a7-motion/butterfly.cpp
@@ -13,7 +13,7 @@ public:
 
    void setup() {
       Joint* body = new Joint("Body");
-      body->setLocalTranslation(vec3(1,2,1)*100.0f);
+      body->setLocalTranslation(vec3(100,200,0));
       body->setLocalRotation(glm::angleAxis(glm::radians<float>(45.0f), vec3(0,1,0)));
       skeleton.addJoint(body);
 
@@ -29,33 +29,62 @@ public:
    }
 
    void scene() {
+       theta += dt();
       Joint* body = skeleton.getByName("Body");
+      body->setLocalTranslation(vec3(100 * sin(elapsedTime()), 100+75*sin(elapsedTime()), 100 * cos(elapsedTime())));
+      body->setLocalRotation(glm::angleAxis(theta, vec3(0, 1, 0)));
+
       Joint* lwing = skeleton.getByName("LWing");
       lwing->setLocalRotation(glm::angleAxis(sin(elapsedTime()), vec3(0,0,1)));
 
       Joint* rwing = skeleton.getByName("RWing");
       rwing->setLocalRotation(glm::angleAxis(-sin(elapsedTime()), vec3(0,0,1))); 
+
+
       skeleton.fk();
+
+
+      // local axes
+      vec3 origin= vec3(0);
+      vec3 xAxis = vec3(100, 0, 0);
+      vec3 yAxis = vec3(0, 100, 0);
+      vec3 zAxis = vec3(0, 0, 100);
+
 
       // attach geometry to skeleton 
       Transform B = body->getLocal2Global(); 
       Transform LT = lwing->getLocal2Global(); 
       Transform RT = rwing->getLocal2Global(); 
 
+      origin = B.transformPoint(origin);
+      xAxis = B.transformPoint(xAxis);
+      yAxis = B.transformPoint(yAxis);
+      zAxis = B.transformPoint(zAxis);
+
       // draw body
       Transform bodyGeometry(
          glm::angleAxis(glm::pi<float>()*0.5f, vec3(1,0,0)), // rotation
          vec3(0), vec3(25, 200, 25)); // position, scale
 
-      Transform lwingGeometry(
-         eulerAngleRO(XYZ, vec3(0,0,0)),
-         vec3(-80,0,0), 
+      Transform lwingGeometry1(
+          glm::angleAxis(glm::pi<float>() * 0.25f, vec3(0, 1, 0)),
+         vec3(-100,0,-50), 
          vec3(120,20,200));
 
-      Transform rwingGeometry(
-         eulerAngleRO(XYZ, vec3(0,0,0)),
-         vec3(80,0,0), 
-         vec3(120,20,200));
+      Transform lwingGeometry2(
+          glm::angleAxis(glm::pi<float>() * -0.25f, vec3(0, 1, 0)),
+          vec3(-100, 0, 50),
+          vec3(120, 20, 200));
+
+      Transform rwingGeometry1(
+          glm::angleAxis(glm::pi<float>() * 0.25f, vec3(0, 1, 0)),
+          vec3(100, 0, 50),
+          vec3(120, 20, 200));
+
+      Transform rwingGeometry2(
+          glm::angleAxis(glm::pi<float>() * -0.25f, vec3(0, 1, 0)),
+          vec3(100, 0, -50),
+          vec3(120, 20, 200));
 
       setColor(vec3(0.4, 0.4, 0.8));
       push();
@@ -63,21 +92,51 @@ public:
       drawSphere(vec3(0), 1);
       pop();
 
-      setColor(vec3(0.8, 0, 0.0));
+      setColor(vec3(1, 153.0f/255.0f, 51.0f/255.0f));
       push();
-      transform(LT * lwingGeometry);
+      transform(LT * lwingGeometry1);
       drawSphere(vec3(0), 1);
       pop();
 
-      setColor(vec3(0, 0.8, 0.0));
+      setColor(vec3(0));
       push();
-      transform(RT * rwingGeometry);
+      transform(LT * lwingGeometry2);
       drawSphere(vec3(0), 1);
       pop();
+
+      setColor(vec3(0));
+      push();
+      transform(RT * rwingGeometry1);
+      drawSphere(vec3(0), 1);
+      pop();
+
+      setColor(vec3(1, 153.0f / 255.0f, 51.0f / 255.0f));
+      push();
+      transform(RT * rwingGeometry2);
+      drawSphere(vec3(0), 1);
+      pop();
+
+      if (show) {
+          setColor(vec3(1, 0, 0));
+          drawEllipsoid(origin, xAxis, 10);
+          setColor(vec3(0, 1, 0));
+          drawEllipsoid(origin, yAxis, 10);
+          setColor(vec3(0, 0, 1));
+          drawEllipsoid(origin, zAxis, 10);
+      }
+   }
+
+   virtual void keyUp(int key, int mods) {
+       if (key == 'A') {
+           show = !show;
+       }
    }
 
 private:
    Skeleton skeleton;
+   float theta = 0;
+   bool show= false;
+
 };
 
 

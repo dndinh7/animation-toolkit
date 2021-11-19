3d7da7e0d00a80ec758eb91fee0f4afc35f49f09
David Dinh <dndinh7@gmail.com>
2021-11-19 11:33:59 -0500

    finished updating character!

--- assignments/a9-fade/controller.cpp
+++ assignments/a9-fade/controller.cpp
@@ -19,6 +19,11 @@ public:
     reader.load("../motions/Beta/walking.bvh", _skeleton, _walk);
     _drawer.showAxes = true;
     pos = vec3(_walk.getKey(0).rootPos.x, 0, _walk.getKey(0).rootPos.z);
+    _walk.update(_skeleton, 0);
+
+    globalPos = _skeleton.getByName("Beta:Head")->getGlobalTranslation();
+    
+    globalLookPos = _skeleton.getByName("Beta:Spine1")->getGlobalTranslation();
   }
 
   virtual void scene()
@@ -35,7 +40,7 @@ public:
     translate(p);
     rotate(_heading, vec3(0, 1, 0));
     translate(vec3(0,0,25));
-    scale(vec3(10, 10, 50));
+    scale(vec3(2, 2, 75));
     drawCylinder(vec3(0), 1.0f);
     pop();
   }
@@ -46,16 +51,20 @@ public:
 
     // TODO: Your code here
     Pose pose= _skeleton.getPose();
-    pose.rootPos = pos + vec3(0, pose.rootPos.y, 0);
+    
     pose.jointRots[0] = glm::angleAxis(_heading, vec3(0, 1, 0));
+    pose.rootPos = pos + vec3(0, pose.rootPos.y, 0);
     _skeleton.setPose(pose);
 
+    pos += vec3(speed*sin(_heading), 0, speed*cos(_heading));
+    globalPos += vec3(speed * sin(_heading), 0, speed * cos(_heading));
+    globalLookPos += vec3(speed * sin(_heading), 0, speed * cos(_heading));
     
-    vec3 globalHeadPos = _skeleton.getByName("Beta:Head")->getGlobalTranslation();
-    vec3 globalPosCam = globalHeadPos + vec3(0, 100, -100);
-
+    vec3 globalPosCam = vec3(globalPos.x, globalPos.y,
+        globalPos.z) + vec3(-200 * sin(_heading), 75, -200 * cos(_heading));
+    
     // TODO: Override the default camera to follow the character
-    lookAt(globalPosCam, globalHeadPos, vec3(0, 1, 0));
+    lookAt(globalPosCam, globalLookPos, vec3(0, 1, 0));
 
     // update heading when key is down
     if (keyIsDown('D')) _heading -= 0.05;
@@ -65,6 +74,11 @@ public:
 protected:
   float _heading= 0.0f;
   vec3 pos;
+  float speed= 0.5f;
+
+  vec3 globalPos;
+  vec3 globalLookPos;
+  bool change = true;
 
   Motion _walk;
   Skeleton _skeleton;

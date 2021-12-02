f632236be83f745efd5a3e48a323bea424b625e9
David Dinh <dndinh7@gmail.com>
2021-12-01 21:51:38 -0500

    half implemented analytic, but got a bug

--- assignments/a10-ik/ik_controller.cpp
+++ assignments/a10-ik/ik_controller.cpp
@@ -26,7 +26,56 @@ bool IKController::solveIKAnalytic(Skeleton& skeleton,
 
   Joint* hip = knee->getParent();
 
+
   // TODO: Your code here
+  Joint* j3 = skeleton.getByID(jointid);
+
+  Joint* j2 = j3->getParent();
+
+  Joint* j1 = j2->getParent();
+
+  float l1 = length(j3->getLocalTranslation());
+  float l2 = length(j2->getLocalTranslation());
+
+  float r = length(goalPos - j1->getGlobalTranslation());
+
+  float cphi = (r * r - l1 * l1 - l2 * l2) / (-2.0f * l1 * l2);
+
+  cphi = std::max(std::min(cphi, 1.0f), -1.0f); // clamps cphi
+  float phi = acos(cphi);
+
+  float theta2z = phi - pi<float>();
+
+  vec3 limbDir = normalize(j2->getLocalTranslation());
+  vec3 axis = cross(limbDir, vec3(0, 0, -1));
+  axis = (limbDir[1] < 0) ? cross(limbDir, vec3(0, 0, 1)) : axis;
+
+  quat R21 = angleAxis(theta2z, axis);
+
+  skeleton.getByID(j2->getID())->setLocalRotation(R21);
+
+  skeleton.fk();
+
+  vec3 p30 = j3->getGlobalTranslation();
+
+  vec3 r_bar = p30 - j1->getGlobalTranslation();
+
+
+  vec3 e_bar = goalPos - p30;
+
+  float cphi2 = length(cross(r_bar, e_bar));
+
+  float phi2 = atan2(length(cross(r_bar, e_bar)), dot(r_bar, r_bar) + dot(r_bar, e_bar));
+
+  vec3 axis2 = normalize(cross(r_bar, e_bar));
+
+  quat R10 = angleAxis(phi2, axis2);
+
+  skeleton.fk();
+
+  if (length(j3->getGlobalTranslation()) - length(goalPos) > epsilon) return false;
+
+
   return true;
 }
 
@@ -37,5 +86,8 @@ bool IKController::solveIKCCD(Skeleton& skeleton, int jointid,
   if (chain.size() == 0) return true;
 
   // TODO: Your code here
+  
+  std::cout << skeleton.getByID(jointid)->getLocalTranslation() << std::endl;
+
   return false;
 }
